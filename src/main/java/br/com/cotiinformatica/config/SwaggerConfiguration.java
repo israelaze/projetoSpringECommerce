077da5b39bf432899360d247c7619f2eabd9e93c
israelaze <israelaze@gmail.com>
2021-07-13 21:45:45 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/config/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.cotiinformatica.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration //classe de configuração do Spring
+@EnableSwagger2 //habilitando o swagger
+public class SwaggerConfiguration {
+
+	//método para configurar a geração da documentação..
+	@Bean
+	public Docket api() {
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}	
+}
+
+
